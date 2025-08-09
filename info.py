08afe66f12719b061a24ba83da41c6722ee18864
TheoLegend <theo.munzwe@gmail.com>
2025-08-09 12:15:37 +0100

    Add files via upload

---
+++ info.py
@@ -0,0 +1,10 @@
+# Personal Information Script for Theo Munzwebiri
+
+full_name = "Theo Munzwebiri"
+height_cm = 149.5
+weight_kg = 38
+
+print("Stored Personal Information:")
+print(f"Full Name: {full_name}")
+print(f"Height: {height_cm} cm")
+print(f"Weight: {weight_kg} kg")
