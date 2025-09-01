a7a9e08cf13a34bf7ed42b67a5f36f8b92f199f8
TheoLegend <theo.munzwe@gmail.com>
2025-09-01 17:48:43 +0100

    Add files via upload

---
+++ os.py
@@ -0,0 +1,43 @@
+#print("hello")
+
+#file=open("abc.txt","r")
+#print(file.read())
+
+
+#file=open("abc.txt","w")
+#file.write("Hi :D")
+
+#with open("abc.txt","w") as file:
+    #file.write("i am Theo")
+
+
+#with open("abc.txt","r") as file:
+    #data=file.readlines()
+    #print(data)
+    #for i in data:
+       #word=i.split()
+       #print(word) 
+
+#with open("abc.txt","r" ) as file:
+    #data1=file.read()
+#with open("xyz.txt","r" ) as file:
+    #data2=file.read()
+
+#data1+="\n"
+#data1+=data2 
+#print("merging file")
+#with open("merging.txt","w")as fp:
+    #fp.write(data1)
+#print("done")
+
+#file=open("abc.txt","r")
+#c=0
+#content=file.read()
+#colist=content.split("\n")
+#for i in colist:
+    #if i:
+        #c+=1
+#print(c)
+#file.close()    
+
+
